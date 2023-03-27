3316f141a84c40f82059f764a210faa41a1ee912
Akash <akashparekh98@gmail.com>
2023-03-27 15:08:54 +0530

    added models - author, post, tag. Updated Views to access data and display data

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return f"{self.full_name()}"
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"#{self.caption}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=80)
+    date = models.DateField()
+    slug = models.SlugField(default="", null=False, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    caption = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,70 +1,18 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Devil",
-        "date": date(2023, 3, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There is nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happend whilst I was enjoying the view!",
-        "content":'''Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab rerum neque a, autem quaerat distinctio velit dignissimos cumque officia ipsum vel sed laborum repellendus, sunt dolor blanditiis, eos consectetur assumenda?
+all_posts = Post.objects.all()
 
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab rerum neque a, autem quaerat distinctio velit dignissimos cumque officia ipsum vel sed laborum repellendus, sunt dolor blanditiis, eos consectetur assumenda?
-
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab rerum neque a, autem quaerat distinctio velit dignissimos cumque officia ipsum vel sed laborum repellendus, sunt dolor blanditiis, eos consectetur assumenda?'''
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(post):
-    return post["date"]
+# def get_date(post):
+#     return post["date"]
 
 # Create your views here.
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = all_posts.order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
@@ -77,7 +25,9 @@ def posts(request):
 
 
 def post_details(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request, "blog/post-detail.html", {
-        "identified_post":identified_post
+        "identified_post":identified_post,
+        "post_tags":identified_post.caption.all()
     })
