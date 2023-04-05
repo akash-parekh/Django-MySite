adb8fcc4a392dea6fdd20fd6652fa0b72c7f7fdb
Akash <akashparekh98@gmail.com>
2023-04-05 17:11:48 +0530

    Added form styling and displayed comments in post details page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,7 +1,10 @@
 from datetime import date
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, DetailView, ListView
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
 from .forms import CommentForm
@@ -27,14 +30,51 @@ class AllPostsView(ListView):
     context_object_name = "posts"
 
 
-class PostDetailView(DetailView):
-    template_name = 'blog/post-detail.html'
-    model = Post
-    context_object_name = "identified_post"
+class PostDetailView(View):
+    
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post":post,
+            "post_tags":post.caption.all(),
+            "comment_form":CommentForm(),
+            "comments":post.comments.all().order_by("-id")
+        }
+        return render(request, 'blog/post-detail.html', context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.caption.all()
-        context["comment_form"] = CommentForm()
-        return context
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
 
+            return HttpResponseRedirect(reverse("post-details-page", args=[slug]))
+        
+        context = {
+            "post":post,
+            "post_tags":post.caption.all(),
+            "comment_form":comment_form,
+            "comments":post.comments.all().order_by("-id")
+        }
+        return render(request, 'blog/post-detail.html', context)
+    
+    # def post(self, request, slug):
+    #     comment_form = CommentForm(request.POST)
+    #     post = Post.objects.get(slug=slug)
+
+    #     if comment_form.is_valid():
+    #       comment = comment_form.save(commit=False)
+    #       comment.post = post
+    #       comment.save()
+          
+    #       return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+    #     context = {
+    #       "post": post,
+    #       "post_tags": post.tags.all(),
+    #       "comment_form": comment_form
+    #     }
+    #     return render(request, "blog/post-detail.html", context)
